15f21c8729c4dfcf81db571c4d318ba708ae6c8d
Wyvrtn <181348033+Wyvrtn@users.noreply.github.com>
2024-09-13 14:04:44 +0000

    Fixed BeatmapSet imports

--- src/main/java/osuapi/models/beatmaps/BeatmapSet.java
+++ src/main/java/osuapi/models/beatmaps/BeatmapSet.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import osuapi.enums.RankedStatus;
 import osuapi.models.Text;
 import osuapi.models.discussions.Discussion;
-import osuapi.models.enums.RankedStatus;
 import osuapi.models.users.User;
 
 @Getter
