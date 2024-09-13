afdf517c275ad0a5a56116859b3cb11450e9315c
Wyvrtn <181348033+Wyvrtn@users.noreply.github.com>
2024-09-13 14:12:08 +0000

    ForumTopic.java imports fixed

--- src/main/java/osuapi/models/forums/ForumTopic.java
+++ src/main/java/osuapi/models/forums/ForumTopic.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import osuapi.models.enums.ForumTopicType;
+import osuapi.enums.ForumTopicType;
 
 @Getter
 @Setter
