7b7b461212af694e87e338c1114d0b6ea8a17bac
Wyvrtn <181348033+Wyvrtn@users.noreply.github.com>
2024-09-13 14:14:21 +0000

    UseStatistics imports fixed

--- src/main/java/osuapi/models/users/UserStatistics.java
+++ src/main/java/osuapi/models/users/UserStatistics.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import osuapi.models.enums.Grade;
+import osuapi.enums.Grade;
 
 @Getter
 @Setter
