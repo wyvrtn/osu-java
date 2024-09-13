be4ac320d14841ef9435c73bb9eb8c66316ce991
Wyvrtn <181348033+Wyvrtn@users.noreply.github.com>
2024-09-13 14:13:15 +0000

    AccountHistoryEntry imports fixed

--- src/main/java/osuapi/models/users/AccountHistoryEntry.java
+++ src/main/java/osuapi/models/users/AccountHistoryEntry.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import osuapi.models.enums.UserAccountHistoryEntryType;
+import osuapi.enums.UserAccountHistoryEntryType;
 
 @Getter
 @Setter
