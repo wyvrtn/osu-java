81cbfca8bcb91c8bfb1637c17d76d72f832a2e76
Wyvrtn <181348033+Wyvrtn@users.noreply.github.com>
2024-09-13 15:17:52 +0000

    OsuApiClient confirmed to work

--- src/main/java/osuapi/app/OsuApiApp.java
+++ src/main/java/osuapi/app/OsuApiApp.java
@@ -3,10 +3,14 @@ package osuapi.app;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+import osuapi.client.OsuApiClient;
+
 @SpringBootApplication
 @Import(OsuApiApp.class)
 public class OsuApiApp {
 	public static void main(String[] args) {
+		OsuApiClient client = new OsuApiClient(1, "secret");
+		System.out.println(client);
 		//Add later SpringApplication.run(OsuApiApp.class); Avoid SonarLint
 	}
 	
