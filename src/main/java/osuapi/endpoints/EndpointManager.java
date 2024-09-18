92b912425b0f200ca8929df2ed5796af97c7a6ed
Wyvrtn <181348033+Wyvrtn@users.noreply.github.com>
2024-09-18 14:12:41 +0000

    Fixed EndpointManager's delgates pointing to null

--- src/main/java/osuapi/endpoints/EndpointManager.java
+++ src/main/java/osuapi/endpoints/EndpointManager.java
@@ -8,25 +8,31 @@ public final class EndpointManager {
 	private OsuApiClient client;
 	
 	@Delegate
-	private BeatmapPacks beatmapPacksDelegate = new BeatmapPacks(client);
+	private BeatmapPacks beatmapPacksDelegate;
 	
 	@Delegate
-	private Beatmaps beatmapsDelegate = new Beatmaps(client);
+	private Beatmaps beatmapsDelegate;
 	
 	@Delegate
-	private BeatmapSets beatmapSetsDelegate = new BeatmapSets(client);
+	private BeatmapSets beatmapSetsDelegate;
 	
 	@Delegate
-	private Changelogs changelogsDelegate = new Changelogs(client);
+	private Changelogs changelogsDelegate;
 	
 	@Delegate
-	private Rankings rankingsDelegate = new Rankings(client);
+	private Rankings rankingsDelegate;
 	
 	@Delegate
-	private Wikis wikisDelegate = new Wikis(client);
+	private Wikis wikisDelegate;
 	
 	private EndpointManager(OsuApiClient client) {
 		this.client = client;
+		beatmapPacksDelegate = new BeatmapPacks(this.client);
+		beatmapsDelegate = new Beatmaps(this.client);
+		beatmapSetsDelegate = new BeatmapSets(this.client);
+		changelogsDelegate = new Changelogs(this.client);
+		rankingsDelegate = new Rankings(this.client);
+		wikisDelegate = new Wikis(this.client);
 	}
 	
 	public static EndpointManager createInstance(OsuApiClient client) {
